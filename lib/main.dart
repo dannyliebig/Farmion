b5276a6f6fe45fc0091a076f79ee56a30f35b4bb
dannyliebig <37117009+dannyliebig@users.noreply.github.com>
2018-04-05 07:43:45 +0200

    Duplicated import
    Commented out duplicated import of material.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:farmion/Dashbord.dart';
 import 'package:flutter/material.dart';
+/* duplicated
 import 'package:flutter/material.dart';
-
+*/
 
 
 void main() => runApp(new MyApp());
